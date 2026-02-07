9b4bc887e0d91e2b8ddd1406c097c6036484a8e7
Abo Sadq <jfrslah211@gmail.com>
2026-02-07 17:52:24 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# توكن البوت الرئيسي
+BOT_TOKEN = "8221274182:AAHUAPfkyv4-n4kmQsmBF5q9vPjTRXn5NTg"
+
+# معلومات الإدارة
+ADMIN_ID = 5580918933 =  # ايديك العددي
+DEV_USER = "@p_p_p1"
+AD_USER = "@xxnnxg"
